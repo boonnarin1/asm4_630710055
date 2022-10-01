5aef2efa19aa942435967083713ad811c4626d0d
Boonnarin Sakboonyarat <underfire878@gmail.com>
2022-10-01 22:00:04 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/asm4_630710055/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asm4_630710055
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:ass_4/home.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Palette.myColor,
+      ),
+      home: const Home(),
+    );
+  }
+}
+class Palette {
+  static const MaterialColor myColor = const MaterialColor(
+    0xffB1B2FF,
+    const <int, Color>{
+      50: const Color(0xff821427), //10%
+      100: const Color(0xff6f1a26), //20%
+      200: const Color(0xff611622), //30%
+      300: const Color(0xff53131d), //40%
+      400: const Color(0xff461018), //50%
+      500: const Color(0xff380d13), //60%
+      600: const Color(0xff2a0a0e), //70%
+      700: const Color(0xff1c060a), //80%
+      800: const Color(0xff0e0305), //90%
+      900: const Color(0xff000000), //100%
+    },
+  );
+}
